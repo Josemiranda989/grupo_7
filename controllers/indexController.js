1e9e5f02b0b07ce0546ab847f185fbe6db03de1b
José Miranda <josemiranda989@gmail.com>
2022-05-24 12:55:32 -0300

    fixed database

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const db = require('../database/models');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
@@ -8,6 +9,7 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	index: (req, res) => {
+
 		res.render('index');
 	},
 
